197ef22f87ebe06094dfb1df512154032b3aea49
Arturs <artuursviignats@gmail.com>
2021-02-24 16:53:23 +0200

    javascript cleaned, need to turn it into a react component

--- app.js
+++ app.js
@@ -1,137 +1,96 @@
-//todo: add sharp gestures to be recognized as a valid swipe
-//todo: clean up code
-//todo: better styles
-
-const slider = document.querySelector('.slider');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
 const carousel = document.querySelector('.carousel');
-const indicatorParents = document.querySelector('.controls ul')
+const slider = document.querySelector('.carousel .slider');
+const prev = document.querySelector('.carousel .controls .prev');
+const next = document.querySelector('.carousel .controls .next');
+const indicatorParents = document.querySelector('.carousel .controls ul');
+const totalPanels = document.querySelectorAll('.carousel .slider section').length;
 
-let direction = 'forward';
-let currentPanel = 0;
+slider.style.width = `${100* totalPanels}%`; //width of the slider is dependant on the amount of panels (<section>s) we have
 
-let lastPanel = 3;
+for(let i = 1; i< totalPanels; i++){
+    indicatorParents.innerHTML += `<li></li>`
+}
 
-let skipMoreThanOnePanel = null;
-
-let isMouseMoving = false;
-let isMouseDown = false;
-let mouseStartPos = 0;
-let transform = 0;
+let MultiplePanelsToSkip = null;
+let isSwipingEnabled = true;
+let isGestureActive = false;
+let gestureStartPosition = 0;
 let transformVal = 0;
-let lastPageX = 0;
-let fullSwipe = null;
+let validSwipe = null;
+let currentPanel = 0;
+let isTransitionendEventEnabled = true;
+let transform = 0;
 let deltaX = 0;
-let transitionEndBlock = false;
-let ignore = false;
+let direction = -1;
 
-const gestureStart = (e) => {
-    isMouseDown = true;
-    mouseStartPos = e.pageX;
-    transform = window.getComputedStyle(slider).getPropertyValue('transform');
-    if (transform !== 'none') {
-        transform = transform.split(',')[4].trim();
-    } else {
-        transform = 0;
-    }
-}
-const gestureMove = (e) => {
-    if (!isMouseDown) {
-        return
-    }
-    if (isMouseDown) {
-        deltaX = e.pageX - mouseStartPos;
-
-        if (deltaX > document.querySelector('section').offsetWidth / 4) {
-            fullSwipe = 'prev';
-        } else if (deltaX < document.querySelector('section').offsetWidth / -4) {
-            fullSwipe = 'next';
-        } else {
-            fullSwipe = null;
-        }
-
-        if (Math.sign(deltaX) === -1) {
-            loadContentFront();
-        } else if (Math.sign(deltaX) === 1) {
-            loadContentBack();
-        }
-
-        transformVal = Number(transform) + deltaX;
-        slider.style.transition = `none`;
-        slider.style.transform = `translateX(${transformVal}px)`;
-        setTimeout(() => {
-            slider.style.transition = `all 0.3s`;
-        });
-    }
-    lastPageX = e.pageX
-}
-const gestureEnd = (e) => {
-    isMouseDown = false;
-    if(!fullSwipe){
-        ignore = true;
-        slider.style.transform = `translateX(0px)`;
-    } else {
-        if(fullSwipe==='next'){
-            slider.style.transition = `0.3s`
-            slider.style.transform = `translate(-${25}%)`;
-        } else if(fullSwipe === 'prev'){
-            slider.style.transition = `0.3s`
-            slider.style.transform = `translate(${25}%)`;
-        }
-    }
+function disableSwiping() {
+    isSwipingEnabled = false;
 }
 
-
-const append = (numOfpanelsToAdd = 1) => {
-    for (let i = 0; i < numOfpanelsToAdd; i++) {
-        slider.appendChild(slider.firstElementChild);
-    }
+function enableSwiping() {
+    isSwipingEnabled = true;
 }
 
-const prepend = (numOfpanelsToAdd = 1) => {
-    for (let i = 0; i < numOfpanelsToAdd; i++) {
-        slider.prepend(slider.lastElementChild)
-    }
+function translate(direction, amount = (100/totalPanels), units = '%') {
+    slider.style.transform = `translate(${direction * amount}${units})`;
 }
 
-const loadContentFront = () => {
-    if (direction === 'backward') {
-        direction = 'forward'
+function loadContent(value) {
+    if (value === 1 && direction === -1) { //if the values dont match, that means there is no content in the direction we want to go. 
+        append()
+        direction = 1;
+        carousel.style.justifyContent = `flex-end`
+    } else if (value === -1 && direction === 1) {
+        direction = -1;
         prepend()
         carousel.style.justifyContent = `flex-start`;
     }
 }
 
-const loadContentBack = () => {
-    if (direction === 'forward') {
-        append()
-        direction = 'backward';
-        carousel.style.justifyContent = `flex-end`
+function append(numOfpanelsToAdd = 1) {
+    for (let i = 0; i < numOfpanelsToAdd; i++) {
+        slider.appendChild(slider.firstElementChild);
     }
 }
 
-// select slide X event
-document.querySelectorAll('.controls li').forEach((indicator, ind) => {
-    indicator.addEventListener('click', e => {
-        if (ind !== currentPanel) {
-            let deltaPanels = (currentPanel - ind);
-            if (Math.sign(deltaPanels) === -1) { //next
-                loadContentFront()
-                slider.style.transform = `translate(${25 * deltaPanels}%)`;
-            } else if (Math.sign(deltaPanels) === 1) { //prev
-                loadContentBack()
-                slider.style.transform = `translate(${25 * deltaPanels}%)`;
+function prepend(numOfpanelsToAdd = 1) {
+    for (let i = 0; i < numOfpanelsToAdd; i++) {
+        slider.prepend(slider.lastElementChild)
+    }
+}
 
-            }
+//arrow(prev/next) click events
+prev.addEventListener('click', e => {
+    disableSwiping();
+    loadContent(1);
+    translate(1);
+})
+
+next.addEventListener('click', e => {
+    disableSwiping();
+    loadContent(-1);
+    translate(-1);
+})
+
+// select slide X event
+document.querySelectorAll('.controls li').forEach((indicator, index) => {
+    indicator.addEventListener('click', e => {
+        if (index !== currentPanel) {
+            disableSwiping()
+            let deltaPanels = (currentPanel - index); //calculate the difference between the current panel, and the panel the user wants to skip to
+
+            loadContent(Math.sign(deltaPanels))
+            translate(deltaPanels)
+
+            //todo - what this-------------
             if (deltaPanels > 1 || deltaPanels < -1) {
-                skipMoreThanOnePanel = deltaPanels;
+                MultiplePanelsToSkip = deltaPanels;
             }
         }
     })
 })
 
-//touch/pointer events
+//touch/pointer/swipe events
 if (window.PointerEvent) {
     slider.addEventListener('pointerdown', gestureStart);
     slider.addEventListener('pointermove', gestureMove);
@@ -141,45 +100,88 @@ if (window.PointerEvent) {
     slider.addEventListener('touchmove', gestureMove);
     slider.addEventListener('touchup', gestureEnd)
 }
-//arrow click events
-prev.addEventListener('click', e => {
-    loadContentBack();
-    slider.style.transform = `translate(${25}%)`;
-})
 
-next.addEventListener('click', e => {
-    loadContentFront();
-    slider.style.transform = `translate(-${25}%)`;
-})
+function gestureStart(e) {
+    if (isSwipingEnabled) {
+        isGestureActive = true; //can be disabled by pointerup/touchup events
+        gestureStartPosition = e.pageX;
 
+        //transform is needed for GestureMove() for the panel swipe-follow animation to work
+        transform = window.getComputedStyle(slider).getPropertyValue('transform');
+        if (transform !== 'none') {
+            transform = transform.split(',')[4].trim();
+        } else {
+            transform = 0;
+        }
+    }
+
+}
+
+function gestureMove(e) {
+    if (isSwipingEnabled && isGestureActive) {
+        deltaX = e.pageX - gestureStartPosition; //calc the difference between the starting position (gestureStartPosition) and current position (e.pageX)
+
+        //depending on the direction of the swipe, load content either in the front or back
+        loadContent(Math.sign(deltaX))
+
+        if (deltaX > document.querySelector('section').offsetWidth / 4) { // if deltaX is more than 1/4 of the panel's width, then it is a valid swipe
+            validSwipe = 1;
+        } else if (deltaX < document.querySelector('section').offsetWidth / -4) {
+            validSwipe = -1;
+        } else {
+            validSwipe = null;
+        }
+
+        //animate the panels so it follows the swipe gesture
+        transformVal = Number(transform) + deltaX;
+        slider.style.transition = `none`;
+        translate(1, transformVal, 'px');
+    }
+}
+
+function gestureEnd(e) {
+    if (isSwipingEnabled) {
+        isGestureActive = false; //can be enabled by pointerdown/touchdown events
+        disableSwiping();
+        if (validSwipe) {
+            slider.style.transition = `0.3s`;
+            translate(validSwipe);
+        } else { //small swipe = reset back to current panel (rubber band effect)
+            isTransitionendEventEnabled = false;
+            slider.style.transition = `all 0.3s`;
+            translate(0);
+        }
+        setTimeout(() => {
+            enableSwiping();
+        }, 300); //todo set to transitionend block??
+    }
+}
 
 slider.addEventListener('transitionend', e => {
-    if (ignore) {
-        ignore = false;
+    if (!isTransitionendEventEnabled) {
+        isTransitionendEventEnabled = true;
         return
     }
-    transitionEndBlock = true;
-    //this block will fire if we do a quick, sharp swipe 
-    if (skipMoreThanOnePanel) {
-        if (Math.sign(skipMoreThanOnePanel) === -1) {//forward
-            append(skipMoreThanOnePanel * -1);
-            currentPanel = currentPanel - skipMoreThanOnePanel;
-        } else if (Math.sign(skipMoreThanOnePanel) === 1) {//backward
-            prepend(skipMoreThanOnePanel);
-            currentPanel = currentPanel - skipMoreThanOnePanel;
+    if (MultiplePanelsToSkip) {
+        if (Math.sign(MultiplePanelsToSkip) === -1) {//forward
+            append(MultiplePanelsToSkip * -1);
+            currentPanel = currentPanel - MultiplePanelsToSkip;
+        } else if (Math.sign(MultiplePanelsToSkip) === 1) {//backward
+            prepend(MultiplePanelsToSkip);
+            currentPanel = currentPanel - MultiplePanelsToSkip;
         }
     } else {
-        if (direction === 'forward') {
+        if (direction === -1) {
             append();
-            if (currentPanel === lastPanel) {
+            if (currentPanel === totalPanels - 1) {
                 currentPanel = 0;
             } else {
                 currentPanel++;
             }
-        } else if (direction === 'backward') {
+        } else if (direction === 1) {
             prepend();
             if (currentPanel === 0) {
-                currentPanel = lastPanel;
+                currentPanel = totalPanels - 1;
             } else {
                 currentPanel--;
             }
@@ -187,7 +189,7 @@ slider.addEventListener('transitionend', e => {
     }
 
     slider.style.transition = `none`;
-    slider.style.transform = `translate(0)`;
+    translate(0)
     setTimeout(() => {
         slider.style.transition = `all 0.3s`;
     });
@@ -195,12 +197,11 @@ slider.addEventListener('transitionend', e => {
     document.querySelector('.controls .selected').classList.remove('selected');
     indicatorParents.children[currentPanel].classList.add('selected');
 
-    if (skipMoreThanOnePanel) {
-        skipMoreThanOnePanel = null;
+    if (MultiplePanelsToSkip) {
+        MultiplePanelsToSkip = null;
     }
-    if (fullSwipe) {
-        fullSwipe = null;
+    if (validSwipe) {
+        validSwipe = null;
     }
-    transitionEndBlock = false;
-
+    enableSwiping();
 })
